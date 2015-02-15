9486e60f51f29604627298933f6cb2c68a63e178
Vivek R <vividvilla@gmail.com>
2015-02-15 17:15:19 +0530

    Added sort by likes feature

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,83 +1,33 @@
 $(document).ready(function() {
-	$('#loadData').click(function() {
+	$('#loadData, #sortLikes').click(function() {
 		$.blockUI({ css: {
 			border: 'none',
 			background: 'transparent',
 			opacity: 1,
 			color: '#fff'
 		},
-		message: '<div class="three-quarters">Loading</div>',
+		message: '<div class="plus">Loading</div>',
 		overlayCSS: { backgroundColor: '#eeeeee' }});
 	});
 });
 
 $(document).ajaxStop($.unblockUI);
 
-function loadAjax() {
-	console.log("Loading")
-	var likesLen = [], titleLen = [];
-	function checkData(test) {
+var dataGlobal = [];
 
-			for (var i=0;i<test.length;i++) {
-				likesLen.push(Number(JSON.stringify(test[i]["likes"])))
-				titleLen.push(JSON.stringify(test[i]["title"]))
-				test[i].y = test[i].likes
-				test[i].url = "http://scn.sap.com" + test[i].link
-				delete test[i].likes
-				delete test[i].link
-			}
-			console.log(JSON.stringify(likesLen))
-			console.log(JSON.stringify(titleLen))
-			console.log(JSON.stringify(test))
-			$('#container').highcharts({
-				chart: {
-					type: 'column'
-				},
-						title: {
-								text: 'Blog',
-								x: -20 //center
-						},
-						subtitle: {
-								text: 'Number of likes per blog ',
-								x: -20
-						},
-				plotOptions: {
-								series: {
-										cursor: 'pointer',
-										point: {
-												events: {
-														click: function () {
-																//location.href = this.options.url;
-																window.open(this.options.url,'_blank')
-														}
-												}
-										}
-								}
-						},
-						series: [{
-								name: 'Likes',
-								data: test
-						}],
-						xAxis: {
-								labels: {
-										enabled: false
-								},
-								categories: titleLen
-						},
-
-				});
-		};
+function loadAjax(callBack, desc) {
 	(function(){
 		$.ajax({
 			url: "/arr/",
 			type: "GET",
 			dataType: "JSON",
 			success: function (data, status, jqXHR) {
-				checkData(data);
+				dataGlobal = data;
+				callBack(data, desc);
 				// execute if success response
 			},
 			error: function (jqXHR, status, err) {
-				console.log(error);
+				console.log(err);
 				// execute if error response
 			},
 			complete: function (jqXHR, status) {
@@ -85,4 +35,80 @@ function loadAjax() {
 			}
 		})
 	})();
+}
+
+function sortData(data, key, desc) {
+	data.sort(function(a,b) {
+		return parseInt(a[key]) - parseInt(b[key])
+	});
+	return data;
+}
+
+function loadGraph() {
+	loadAjax(plotGraph);
+}
+
+function sortByLikes(desc) {
+	if(dataGlobal.length === 0) {
+		loadAjax(plotSortedLike, desc);
+	} else {
+		plotSortedLike(dataGlobal, desc);
+	}
+}
+
+function plotSortedLike(data, desc) {
+	plotGraph(sortData(data, 'likes'), desc);
+}
+
+function plotGraph(data, desc) {
+	setTimeout($.unblockUI, 500);
+
+	if(desc === true) {
+		data = data.reverse()
+	}
+
+	var likesLen = [], titleLen = [];
+	for (var i=0;i<data.length;i++) {
+		likesLen.push(Number(JSON.stringify(data[i]["likes"])))
+		titleLen.push(JSON.stringify(data[i]["title"]))
+		data[i].y = data[i].likes
+		data[i].url = "http://scn.sap.com" + data[i].link
+	}
+
+	$('#container').highcharts({
+		chart: {
+			type: 'column'
+		},
+		title: {
+			text: 'Blog',
+			x: -20 //center
+		},
+		subtitle: {
+			text: 'Number of likes per blog ',
+			x: -20
+		},
+		plotOptions: {
+			series: {
+				cursor: 'pointer',
+				point: {
+					events: {
+						click: function () {
+							//location.href = this.options.url;
+							window.open(this.options.url,'_blank')
+						}
+					}
+				}
+			}
+		},
+		series: [{
+			name: 'Likes',
+			data: data
+		}],
+		xAxis: {
+			labels: {
+					enabled: false
+			},
+			categories: titleLen
+		},
+	});
 }
